034d832a9bd7bc068bd67acc3fbf191e9231ba0a
Naim-Bijapure <Naimbijapure7407@gmail.com>
2019-03-04 20:32:35 +0530

    updating

--- App.js
+++ App.js
@@ -1,23 +1,123 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+import React, { Component } from 'react';
+import Permissions from 'react-native-permissions';
+import {
+  Button,
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import {
+  NativeEventEmitter,
+  NativeModules
+} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import MyChirp from './app/myChirp';
+const ChirpConnect = NativeModules.ChirpConnect;
+const ChirpConnectEmitter = new NativeEventEmitter(ChirpConnect);
 
-export default class App extends Component {
+const key = 'CHIRP_APPLICATION_KEY';
+const secret = 'CHIRP_APPLICATION_SECRET';
+
+export default class App extends Component<{}> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      'initialised': false,
+      'status': 'Sleeping',
+      'data': '----------'
+    }
+  }
+
+  async componentDidMount() {
+    const response = await Permissions.check('microphone')
+    if (response !== 'authorized') {
+      await Permissions.request('microphone')
+    }
+
+    this.onStateChanged = ChirpConnectEmitter.addListener(
+      'onStateChanged',
+      (event) => {
+        if (event.status === ChirpConnect.CHIRP_CONNECT_STATE_STOPPED) {
+          this.setState({ status: 'Stopped' });
+        } else if (event.status === ChirpConnect.CHIRP_CONNECT_STATE_PAUSED) {
+          this.setState({ status: 'Paused' });
+        } else if (event.status === ChirpConnect.CHIRP_CONNECT_STATE_RUNNING) {
+          this.setState({ status: 'Running' });
+        } else if (event.status === ChirpConnect.CHIRP_CONNECT_STATE_SENDING) {
+          this.setState({ status: 'Sending' });
+        } else if (event.status === ChirpConnect.CHIRP_CONNECT_STATE_RECEIVING) {
+          this.setState({ status: 'Receiving' });
+        }
+      }
+    );
+
+    this.onReceived = ChirpConnectEmitter.addListener(
+      'onReceived',
+      (event) => {
+        if (event.data.length) {
+          this.setState({ data: event.data });
+        }
+      }
+    )
+
+    this.onError = ChirpConnectEmitter.addListener(
+      'onError', (event) => { console.warn(event.message) }
+    )
+
+    try {
+      ChirpConnect.init(key, secret);
+      await ChirpConnect.setConfigFromNetwork();
+      ChirpConnect.start();
+      this.setState({ initialised: true })
+    } catch(e) {
+      console.warn(e.message);
+    }
+  }
+
+  componentWillUnmount() {
+    this.onStateChanged.remove();
+    this.onReceived.remove();
+    this.onError.remove();
+  }
+
+  onPress() {
+    ChirpConnect.send([0, 1, 2, 3, 4]);
+  }
+
   render() {
-    
     return (
-  <MyChirp></MyChirp>
-       );
-   
-    
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Chirp Connect!
+        </Text>
+        <Text style={styles.instructions}>
+          {this.state.status}
+        </Text>
+        <Text style={styles.instructions}>
+          {this.state.data}
+        </Text>
+      <Button onPress={this.onPress} title='SEND' disabled={!this.state.initialised} />
+      </View>
+    );
   }
 }
 
@@ -31,9 +131,10 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 60,
   },
   instructions: {
+    padding: 10,
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
